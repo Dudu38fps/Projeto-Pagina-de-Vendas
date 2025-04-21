8ee4b80960b9a03dbdab9d24071938c4659d496a
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2025-04-21 20:05:27 -0300

    2 parte

---
+++ script.js
@@ -0,0 +1,32 @@
+const containers = document.querySelectorAll('.pdt')
+const camisasContainer = containers[0]
+const bermudasContainer = containers[1]
+const calcasContainer = containers[2]
+
+function criarCard(produto) {
+    return `
+    <div class = "produto">
+    <img src = "${produto.imagem}" alt="${produto.nome} "width = 150px" "height = 150px">
+    <h4> ${produto.nome}</h4>
+    <p> R$ ${produto.preco.toFixed(2)}</p>
+    </div>
+    `
+}
+
+fetch ('produtos.json')
+.then(res=> res.json())
+.then(produtos=> {
+    produtos.forEach(produto => {
+        const card = criarCard(produto)
+
+        if (produto.id === 1) {
+            camisasContainer.innerHTML += card
+        } else if (produto.id === 2 ) {
+            bermudasContainer.innerHTML += card
+        }else if (produto.id === 3 ) {
+            calcasContainer.innerHTML += card
+        }
+    });
+})
+
+    
